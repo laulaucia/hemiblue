c6314d88e0e9621c7561cb2447d279d7055c9aed
laulaucia <buitrago.laura@gmail.com>
2020-05-20 14:02:30 -0700

    adding modal script

--- js/scripts.js
+++ js/scripts.js
@@ -30,4 +30,24 @@ var primarySlider = new Splide( '#primary-slider', {
 
 // Set the thumbnails slider as a sync target and then call mount.
 primarySlider.sync( secondarySlider ).mount();
+
+// modal stuff
+    
+    var modal = document.getElementById("myModal");
+    var span = document.getElementsByClassName("close")[0];
+    
+    setTimeout(function(){ modal.style.display = "block";}, 3000);
+    
+        // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+	
 	} );
