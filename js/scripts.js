7610ee3dde763557a4bf574c4415e6275ee946c4
laulaucia <buitrago.laura@gmail.com>
2020-05-20 00:23:35 -0700

    adding splide code

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,35 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+document.addEventListener( 'DOMContentLoaded', function () {
+
+var secondarySlider = new Splide( '#secondary-slider', {
+	rewind      : true,
+	fixedWidth  : 100,
+	fixedHeight : 64,
+	isNavigation: true,
+	gap         : 10,
+	focus       : 'center',
+	pagination  : false,
+	cover       : true,
+	breakpoints : {
+		'600': {
+			fixedWidth  : 66,
+			fixedHeight : 40,
+		}
+	}
+} ).mount();
+
+// Create the main slider.
+var primarySlider = new Splide( '#primary-slider', {
+	type       : 'fade',
+	heightRatio: 0.5,
+	pagination : false,
+	arrows     : false,
+	cover      : true,
+} );
+
+// Set the thumbnails slider as a sync target and then call mount.
+primarySlider.sync( secondarySlider ).mount();
+	} );
